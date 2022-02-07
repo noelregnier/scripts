1bbf0a037e6fe132bb293cc4c0c7342b5b944ec6
Katerina Dedova <noel.regnier@gmail.com>
2022-02-07 12:21:17 +0300

    Added saving the results to a file

--- variants_of_name_Yana.py
+++ variants_of_name_Yana.py
@@ -8,31 +8,29 @@ content = res.text
 
 # links = re.findall('.*<a .*href="(.+?)">Я.+?', content)
 links = re.findall('<div class="one_name">.*<a .*href="(.+?)">Я.+?</div>', content)
-print(links)
+# print(links)
 links = ["https://imya.com/" + link for link in links]
 print(len(links))
 derivatives = []
 names = []
+
 for k, link in enumerate(links):
+
     print(k, link)
     if "name" not in link:
         continue
     res_link = requests.get(link)
     # print("trying ", link)
     content_link = res_link.text
-    pages_body_text = re.findall('<div class="pages_body_text">(.*Яна.*)</div>', content_link)
+    pages_body_text = re.findall('<div class="pages_body_text">(.*Яна[^a-z]+?)</div>', content_link)
     if pages_body_text:
         name = re.findall("<h1>(.+?)</h1>", content_link)
-        names.append((name[0], link))
-        print(name, link)
-for i in names:
-    print(i)
-    # print(pages_body_text)
-#     if "Яна" in content_link:
-#         derivatives.append(link)
-#         print("it suits")
-#         name = re.findall("<h1>(.+?)</h1>", content_link)
-#         print(name)
-#
-# print("derivatives are:")
-# print(derivatives)
+        names.append((name[0].split()[1], link))
+        print(names)
+# for i in names:
+#     print(i)
+
+with open("yana_names_reduced.txt", "w") as f:
+    for line in names:
+        f.write(" ".join(line) + "\n")
+
